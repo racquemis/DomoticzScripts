289896a7f1c9857a45b7389e58c87c90bf8e93fe
racquemis <racquemis@users.noreply.github.com>
2017-01-12 10:14:49 +0100

    Update GeoLocation.lua
    Changed user variable name

--- scripts/lua/Location/GeoLocation.lua
+++ scripts/lua/Location/GeoLocation.lua
@@ -15,13 +15,11 @@ function round(num, idp)
   return math.floor(num * mult + 0.5) / mult
 end
 
-
 commandArray = {}
 if (string.len(tostring(uservariablechanged['Location [Jasper]'])) > 3) then
-	print('change detected')
-	print(uservariables['Home Location'])
+	print('change detected: '..uservariables['Home Location'])
 	home_latitude, home_longitude = uservariables['Home Location']:match("([^,]+),([^,]+)")
-	latitude, longitude = uservariablechanged['Location [Jasper]']:match("([^,]+),([^,]+)")
+	latitude, longitude = uservariablechanged['Location [NAME]']:match("([^,]+),([^,]+)")
 	dlon = math.rad(home_longitude-longitude)
 	dlat = math.rad(home_latitude-latitude)
 	sin_dlat = math.sin(dlat/2)
